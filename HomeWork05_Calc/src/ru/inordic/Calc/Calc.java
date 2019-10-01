8f6218bf9a10eb7ba07e7038428fd03274ae9566
Pavel Ekshin <p.ekshin@pekshin-nb.cti.ru>
2019-10-01 18:10:25 +0300

    18.10

--- HomeWork05_Calc/src/ru/inordic/Calc/Calc.java
+++ HomeWork05_Calc/src/ru/inordic/Calc/Calc.java
@@ -4,132 +4,129 @@ import java.util.Scanner;
 
 public class Calc {
 
-
-public static boolean checkDigitInput(String str) {
-	    try {
-	        Double.valueOf(str);
-	        return true;
-	    } catch (NumberFormatException e) {
-	    	System.out.println("This is not a double type!");
-	        return false;
-	    }
-}	
-
-public static boolean checkOperator (String operator) {
-if (operator.matches("[-+*^/]|mod|div"))
-	return true;
-else
-	return false;
-}
-
-public static double sum (double arg1, double arg2) {
-	return arg1+arg2;
-}
-
-public static double sub (double arg1, double arg2) {
-	return arg1-arg2;
-}
-
-public static double pow (double arg1, double arg2) {
-	double pow = 0;
-	if (arg2 > 0) {
-		for (int i=1; i<arg2; i++) {
-			pow = arg1 *= arg1;		
-			}
-		return pow;
+	public static boolean checkDigitInput(String str) {
+		try {
+			Double.valueOf(str);
+			return true;
+		} catch (NumberFormatException e) {
+			System.out.println("This is not a double type!");
+			return false;
+		}
 	}
-	else if (arg2 == 0) {
-		return pow = 1;
+
+	public static boolean checkOperator(String operator) {
+		if (operator.matches("[-+*^/]|mod|div"))
+			return true;
+		else
+			return false;
+	}
+
+	public static double sum(double arg1, double arg2) {
+		return arg1 + arg2;
+	}
+
+	public static double sub(double arg1, double arg2) {
+		return arg1 - arg2;
+	}
+
+	public static double pow(double arg1, double arg2) {
+		double pow = 0;
+		if (arg2 > 0) {
+			for (int i = 1; i < arg2; i++) {
+				pow = arg1 *= arg1;
 			}
-	else return pow;
-}
+			return pow;
+		} else if (arg2 == 0) {
+			return pow = 1;
+		} else
+			return pow;
+	}
 
-public static int divideInt (double arg1, double arg2) {
-	return (int)arg1/(int)arg2;
-}
+	public static int divideInt(double arg1, double arg2) {
+		return (int) arg1 / (int) arg2;
+	}
 
-public static double mod (double arg1, double arg2) {
-	return arg1 % arg2;
-}
+	public static double mod(double arg1, double arg2) {
+		return arg1 % arg2;
+	}
 
-public static double multipli (double arg1, double arg2) {
-	return arg1 * arg2;
-}
+	public static double multipli(double arg1, double arg2) {
+		return arg1 * arg2;
+	}
 
-public static double divide (double arg1, double arg2) {
-	return arg1 / arg2;
-}
+	public static double divide(double arg1, double arg2) {
+		return arg1 / arg2;
+	}
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		double arg1 = 0;
 		double arg2 = 0;
 		String argOper = "";
-		
+
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("The code make math operations on DOUBLE type value.");
-		
+
 		boolean running = true;
-		
-		while (running) {	
-		System.out.println("Please enter first digit in new line:");
-		String digit1 = scanner.nextLine();
-		if (checkDigitInput(digit1)) {
-			running = false;
-			arg1 = Double.parseDouble(digit1);
+
+		while (running) {
+			System.out.println("Please enter first digit in new line:");
+			String digit1 = scanner.nextLine();
+			if (checkDigitInput(digit1)) {
+				running = false;
+				arg1 = Double.parseDouble(digit1);
 			}
 		}
-		
+
 		running = true;
-		
-		while (running) {	
+
+		while (running) {
 			System.out.println("Please enter second digit in new line:");
-			String  digit2 = scanner.nextLine();
+			String digit2 = scanner.nextLine();
 			if (checkDigitInput(digit2)) {
 				running = false;
 				arg2 = Double.parseDouble(digit2);
 			}
 		}
-		
+
 		running = true;
 
-		while (running) {	
+		while (running) {
 			System.out.println("Enter math operator (+,-,/,*,^,div,mod) in new line:");
 			String operator = scanner.nextLine().toLowerCase();
 			if (checkOperator(operator)) {
-				if ((operator.equals("/") && arg2 == 0) || (operator.equals("div") && arg2 == 0) ){
+				if ((operator.equals("/") && arg2 == 0) || (operator.equals("div") && arg2 == 0)) {
 					System.out.println("You cannot divide by zero!!!. Please enter another operator.");
-				}
-				else {
+				} else {
 					running = false;
-					argOper = operator;}
+					argOper = operator;
 				}
-		}
-	
-		
-			switch (argOper) {
-			case "+": 
-				System.out.println("Sum of entered digits: " +sum(arg1,arg2));
-				break;
-			case "-": 
-				System.out.println("Sub of entered digits: " +sub(arg1,arg2));
-				break;
-			case "*": 
-				System.out.println("Multipli of entered digits: " +multipli(arg1,arg2));
-				break;
-			case "/": 
-				System.out.println("Divide of entered digits: " +divide(arg1,arg2));
-				break;
-			case "div": 
-				System.out.println("Div of entered digits: " +divideInt(arg1,arg2));
-				break;
-			case "mod": 
-				System.out.println("Mod of entered digits: " +mod(arg1,arg2));
-				break;
-			case "^": 
-				System.out.println("Pow of entered digits: " +pow(arg1,arg2));
-				break;	
-			}	
 			}
+		}
+
+		switch (argOper) {
+		case "+":
+			System.out.println("Sum of entered digits: " + sum(arg1, arg2));
+			break;
+		case "-":
+			System.out.println("Sub of entered digits: " + sub(arg1, arg2));
+			break;
+		case "*":
+			System.out.println("Multipli of entered digits: " + multipli(arg1, arg2));
+			break;
+		case "/":
+			System.out.println("Divide of entered digits: " + divide(arg1, arg2));
+			break;
+		case "div":
+			System.out.println("Div of entered digits: " + divideInt(arg1, arg2));
+			break;
+		case "mod":
+			System.out.println("Mod of entered digits: " + mod(arg1, arg2));
+			break;
+		case "^":
+			System.out.println("Pow of entered digits: " + pow(arg1, arg2));
+			break;
+		}
+	}
 }
