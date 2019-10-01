b57a4e7350abc47557f9171767948dc49af7be3c
Pavel Ekshin <p.ekshin@pekshin-nb.cti.ru>
2019-10-01 12:05:50 +0300

    asd

---
+++ ClassWork05_sumFibonacci/src/ru/inordic/FibonacciSum.java
@@ -0,0 +1,31 @@
+package ru.inordic;
+
+import java.util.Scanner;
+
+public class FibonacciSum {
+
+	public static void main(String[] args) {
+
+		System.out.println("Введите границу цикла [0,n)");
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		scanner.close();
+		
+		int sum = 0;
+		
+		for (int i=0; i <= n; i++)
+		{
+			if (i % 2 == 0) {
+				sum +=i;
+			}
+			else {
+		}
+			
+		}
+		
+		System.out.println("Сумма четных чисел: " +sum);
+
+	}
+
+}
