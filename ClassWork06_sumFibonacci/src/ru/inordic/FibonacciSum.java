3518b456738259309f30db1c398eb75be0342fc3
Pavel Ekshin <p.ekshin@pekshin-nb.cti.ru>
2019-10-02 22:46:32 +0300

    22:46

---
+++ ClassWork06_sumFibonacci/src/ru/inordic/FibonacciSum.java
@@ -0,0 +1,40 @@
+package ru.inordic;
+
+import java.util.Scanner;
+
+public class FibonacciSum {
+
+	public static void main(String[] args) {
+
+		System.out.println("Введите с новой строки число n>1 для расчета суммы числе Фибоначчи Fn = F(n-1) + F(n-2)");
+		Scanner scanner = new Scanner(System.in);
+				
+		int n = scanner.nextInt();
+		scanner.close();
+		
+		int [] arr = new int[n];
+		int sum = 0;
+		
+		for (int i=0; i<n; i++) {
+			arr[i]+=F(i);
+			sum += arr[i];
+		}
+		
+			System.out.println("Сумма чисел Фибоначчи: " +sum);
+		
+		for (int i=0; i<n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		
+	}
+	
+	public static int F(int n) {
+		
+		if (n <= 0)
+			return 0;
+		else if (n == 1) {
+			return 1;
+		}
+			return F(n-1) + F(n-2);
+	}
+}
