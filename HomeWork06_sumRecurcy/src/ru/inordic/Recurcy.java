b783c305c750f316da83447468c00730fb570798
Pavel Ekshin <p.ekshin@pekshin-nb.cti.ru>
2019-10-03 17:07:12 +0300

    13:23

--- HomeWork06_sumRecurcy/src/ru/inordic/Recurcy.java
+++ HomeWork06_sumRecurcy/src/ru/inordic/Recurcy.java
@@ -14,44 +14,16 @@ public class Recurcy {
 		int n = scanner.nextInt();
 		scanner.close();
 
-		System.out.println("Массив: " +Recurcy(n));
-		System.out.println(n+"!: " +Recurcy(n).get(n));
-		System.out.println(n+"!: " +Recurcy2(n));
-		System.out.println(n+"!: " +Recurcy3(n));
-
-	}
-
-	public static ArrayList<Integer> Recurcy (int n) {
-
-		ArrayList<Integer> list = new ArrayList<Integer>(n);
-
-		for (int i = 0; i <= n; i++) {
-			if (i == 0 || i == 1)
-				list.add(i,1);
-		else
-			list.add(i,i*list.get(i-1));
-		}
-		return list;
-	}
 	
-	public static int Recurcy2 (int n) {
+		System.out.println(n+"!: " +Recurcy(n));
 
-		ArrayList<Integer> list = new ArrayList<Integer>(n);
+	}
 
-		for (int i = 0; i <= n; i++) {
-			if (i == 0 || i == 1)
-				list.add(i,1);
-		else
-			list.add(i,i*list.get(i-1));
 		
-		}
-		return list.get(n);
-	}
-	
-	public static int Recurcy3 (int n) {
+	public static int Recurcy (int n) {
 		  if (n == 0 || n == 1) 
 			  return 1;
-		    return n * Recurcy3(n - 1);
+		    return n * Recurcy(n - 1);
 	}
 
 }
