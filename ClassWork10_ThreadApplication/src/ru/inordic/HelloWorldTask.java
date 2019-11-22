f49a88f30a69d7599be5db87dcbb340e93d4c278
Pavel Ekshin <p.ekshin@pekshin-nb.cti.ru>
2019-11-22 16:31:33 +0300

    S

--- ClassWork10_ThreadApplication/src/ru/inordic/HelloWorldTask.java
+++ ClassWork10_ThreadApplication/src/ru/inordic/HelloWorldTask.java
@@ -1,12 +1,26 @@
 package ru.inordic;
 
 public class HelloWorldTask implements Runnable {
+	
+	
+	private int number;
+
+	public HelloWorldTask(int number) {
+		super();
+		this.number = number;
+	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWork!");
+
+	/*	try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			e.getStackTrace();
+		}
+		*/
+		System.out.println("HelloWork! " + number);
 
 	}
-
 }
--- ClassWork10_ThreadApplication/src/ru/inordic/ThreadApplication.java
+++ ClassWork10_ThreadApplication/src/ru/inordic/ThreadApplication.java
@@ -2,13 +2,47 @@ package ru.inordic;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadApplication {
 
-	public static void main(String[] args) {
-		
-	
-		Thread thread = new Thread(new HelloWorldTask());
-		thread.start();
+	public static void main(String[] args) throws InterruptedException {
+
+		AtomicInteger count = new AtomicInteger();
+		ArrayList<Thread> tasks = new ArrayList<Thread>();
+
+		/*
+		 * Thread thread1 = new Thread(new HelloWorldTask(1)); Thread thread2 = new
+		 * Thread(new HelloWorldTask(2)); Thread thread3 = new Thread(new
+		 * HelloWorldTask(3)); Thread thread4 = new Thread(new HelloWorldTask(4));
+		 */
+		/*
+		 * thread1.start(); tasks.add(thread1); thread2.start(); tasks.add(thread2);
+		 * thread3.start(); tasks.add(thread3); thread4.start(); tasks.add(thread4);
+		 */
+
+		for (int i = 0; i < 100; i++) {
+			Thread thread = new Thread(new Runnable() {
+				public void run() {
+					count.getAndIncrement();
+				}
+			});
+			
+			thread.start();
+			tasks.add(thread);
+
+		}
+
+		/*
+		 * try { for (var pool : tasks) { pool.join(1_000); } } catch
+		 * (InterruptedException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); } }
+		 */
+		for (var task : tasks) {
+			task.join();
+		}
+
+		System.out.print(count.get());
+
 	}
 }
